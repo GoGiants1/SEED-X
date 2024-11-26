b99538744933b85841325d81e6a08c109f10a185
GoGiants1 <chooi221@snu.ac.kr>
2024-11-26 20:37:21 +0900

    Upload model to hf

--- src/tools/reload_qwen_vit.py
+++ src/tools/reload_qwen_vit.py
@@ -1,14 +1,29 @@
 import torch
+from huggingface_hub import HfApi
+from torch import nn
 from transformers import AutoModelForCausalLM
 
 torch.manual_seed(1234)
 
-qwen_model_path = 'pretrained/Qwen-VL-Chat'
-save_path = 'pretrained/QwenViT/qwen_vit_G.pt'
+qwen_model_path = "Qwen/Qwen-VL-Chat"
+save_path = "qwen_vit_G.pt"
 
-model = AutoModelForCausalLM.from_pretrained(qwen_model_path, device_map="cpu", trust_remote_code=True).eval()
+model = AutoModelForCausalLM.from_pretrained(
+    qwen_model_path, device_map="cpu", trust_remote_code=True
+).eval()
 
 visual_encoder = model.transformer.visual
 print(visual_encoder)
-
 torch.save(visual_encoder.state_dict(), save_path)
+
+api = HfApi()
+api.upload_file(
+    path_or_fileobj=save_path,
+    path_in_repo="qwen_vit_G.pt",
+    repo_id="GoGiants1/QwenViT",
+    repo_type="model",
+)
+
+# Save the visual encoder in local disk
+
+# Save the visual encoder in huggingface hub
